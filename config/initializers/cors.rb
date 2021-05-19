785ee186acda5fea05605bb56f02c20fb586025d
Stacey McKnight <mcknight.stm@gmail.com>
2021-05-19 10:11:34 -0500

    dynamically set allowed rack-cors origins based on env

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'deploy-learning.netlify.app'
+    origins Rails.application.config.allowed_cors_origins
 
     resource '*',
       headers: :any,
